5a3948c672eb345f1a2e45ce0727e50ac1767305
omermunk <omermunk95@gmail.com>
2024-08-19 17:05:23 -0700

    using the correct package

--- IroneDomeAPI_v1/Controllers/UserController.cs
+++ IroneDomeAPI_v1/Controllers/UserController.cs
@@ -1,20 +1,35 @@
 using IroneDomeAPI_v1.Models;
 using Microsoft.AspNetCore.Mvc;
 
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
+
+using System.IdentityModel.Tokens.Jwt;
+
+
 namespace IroneDomeAPI_v1.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
 public class UserController : ControllerBase
 {
+    private string GenerateToken()
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        return "";
+    }
+    
     [HttpPost("login")]
     public IActionResult Login(LoginObject loginObject)
     {
         if (loginObject.UserName == "admin" &&
             loginObject.Password == "123456")
         {
+            
+            
             return StatusCode(200
-                ,new {token = "SHUBULU_TOKEN"}
+                ,new {token = GenerateToken()}
                 );
         }
         return StatusCode(StatusCodes.Status401Unauthorized,
